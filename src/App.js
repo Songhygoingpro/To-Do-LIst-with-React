2d2b15cb41776072d85c6c6bbec9a471937bc27b
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-05-08 19:24:50 +0700

    'first'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import To_do_list from './components/To-Do-List';
+
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center h-[100vh] bg-sky-500 md:bg-white">
+      <To_do_list />
+  
+    </div>
+  );
+}
+
+export default App;
